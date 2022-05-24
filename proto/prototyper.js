5da18944b4f068525cae6d437a5804975980eb44
Chris Lynch <github@planetofthepenguins.com>
2022-05-24 10:19:39 +0100

    Finally fixed callbacks

--- proto/prototyper.js
+++ proto/prototyper.js
@@ -1,77 +1,88 @@
 const sqlite3 = require('sqlite3').verbose();
 const { v1: uuidv1, v4: uuidv4, } = require('uuid');
+/*
 var db = new sqlite3.Database(':memory:');
+*/
+const options = {};
 
-async function tableExists(table){
-    var ret = false
-    db.serialize(() => {
-        db.get("SELECT name FROM sqlite_master WHERE type='table' AND name='" + table + "'",
-            (err,row) => {
-                if(row == undefined){
-                    ret = false
-                } else {
-                    if(row.name == table){
-                        ret = true
-                    } else {
-                        ret = false
-                    }
-                }
-                console.log('tableExists(' + table + '): ' + ret)
-                return ret
-            })
+function path(newPath){
+    if(newPath !== undefined){
+        options.path = newPath
+    }
+    return options.path
+}
+
+function tableExists(table,callback){
+    db.get("SELECT name FROM sqlite_master WHERE type='table' AND name='" + table + "'",
+    (err,row) => {
+        if(row == undefined){
+            callback(false)
+        } else {
+            if(row.name == table){
+                callback(true)
+            } else {
+                callback(false)
+            }
         }
-    )
+        // console.log('tableExists(' + table + '): ' + ret)
+    })
 }
 
-async function connect(path) {
-    db.close()
-    db = new sqlite3.Database(path)
-}
-
-function init(){
-    if(tableExists('index') == false){
-        db.serialize(() => {
+function connect(callback) {
+    db = new sqlite3.Database(options.path)
+    console.log('Opened ' + options.path)
+    tableExists('proto',(indexExists) => {
+        if(indexExists == false){
+            console.log('Creating proto table')
             db.run(`
-            CREATE TABLE "index" (
+            CREATE TABLE proto (
                 guid TEXT,
-                "type" TEXT,
-                class TEXT,
-                template TEXT,
-                name TEXT,
-                uri TEXT,
-                code TEXT,
-                status INTEGER,
-                flag INTEGER,
-                rank INTEGER,
-                system INTEGER,
-                domain INTEGER,
-                integration INTEGER,
-                indexation INTEGER,
+                "type" TEXT, class TEXT, template TEXT,
+                name TEXT, uri TEXT, code TEXT,
+                status INTEGER, flag INTEGER, rank INTEGER, system INTEGER,
+                domain INTEGER, integration INTEGER, indexation INTEGER,
                 parent TEXT,
-                created TEXT,
-                updated TEXT,
+                created TEXT, updated TEXT,
                 data TEXT,
                 CONSTRAINT index_PK PRIMARY KEY (guid)
             );
-            `)
-        })
-    }
+            `,undefined,function (err) {
+                if(err == null){
+                    console.log('Table proto created')
+                    callback(db)
+                } else {
+                    callback(undefined,err)
+                }
+            })                
+        } else {
+            console.log('Table proto already exists')
+            callback(db)    
+        }
+    })
 }
 
 function prepare(obj){
     // guid check
     if(obj.guid == '' || obj.guid == undefined){ obj.guid = uuidv1() + '-' + uuidv4() }
     // Type
-
+    if(obj.type == '' || obj.type == undefined) { obj.type = '' } 
     // Class?
-
+    if(obj.class == '' || obj.class == undefined) { obj.class = '' } 
+    // Template?
+    if(obj.template == '' || obj.template == undefined) { obj.template = '' } 
+    // URI?
+    if(obj.uri == '' || obj.uri == undefined) { obj.uri = '' } 
+    // Code?
+    if(obj.code == '' || obj.code == undefined) { obj.code = '' } 
+    // Parent
+    if(obj.parent == '' || obj.parent == undefined) { obj.parent = '' } 
+    
     // Integer defaults
     integerProperties = ['status','flag','rank','system',
                          'domain','integration','integration','indexation']
     for(const i in integerProperties){
         obj[integerProperties[i]] = 0
     }
-
     // Date defaults
     var now = new Date()
     now = now.toISOString()
@@ -81,82 +92,95 @@ function prepare(obj){
     obj.updated = now
 }
 
-function save(obj){
-    // Prepare the object
-    prepare(obj)
-    console.log(obj)
-    return false
-
-    // Save to the database
-    /*
-    db.serialize(() => {
-        const stmt = db.prepare(`
-            INSERT INTO index(
-                guid,
-                type,
-                class,
-                template,
-                name,
-                uri,
-                code,
-                status,
-                flag,
-                rank,
-                system,
-                domain,
-                integration,
-                indexation,
-                parent,
-                created,
-                updated,
-                data
-            ) 
-            VALUES (?)`);
-        stmt.run(
-            obj.guid,
-            obj.type,
-            obj.class,
-            obj.template,
-            obj.name,
-            obj.uri,
-            obj.code,
-            obj.status,
-            obj.flag,
-            obj.rank,
-            obj.system,
-            obj.domain,
-            obj.integration,
-            obj.indexation,
-            obj.parent,
-            obj.created,
-            obj.updated,
-            JSON.stringify(data)
-        );
-        stmt.finalize();
+function save(obj,callback){
+    connect((db,err) => {
+        if(err == undefined){
+            // Prepare the object
+            prepare(obj)
+                
+            // Save to the database
+            db.run(`
+                INSERT INTO proto(
+                    guid,
+                    type, class, template,
+                    name, uri,
+                    code, status, flag, rank,
+                    system, domain, integration, indexation,
+                    parent,
+                    created, updated,
+                    data
+                ) 
+                VALUES (?,
+                        ?,?,?,
+                        ?,?,
+                        ?,?,?,?,
+                        ?,?,?,?,
+                        ?,
+                        ?,?,
+                        ?)`,
+                [
+                    obj.guid,
+                    obj.type, obj.class, obj.template,
+                    obj.name, obj.uri,
+                    obj.code, obj.status, obj.flag, obj.rank,
+                    obj.system, obj.domain, obj.integration, obj.indexation,
+                    obj.parent,
+                    obj.created, obj.updated,
+                    JSON.stringify(obj)
+                ],
+                db.close(() => {
+                    callback(obj)
+                })
+            )
+        }
     })
-    return obj
-    */
 }
 
-function load(guid){
-    /*
-    db.serialize(() => {
-        db.each("SELECT rowid AS id, info FROM lorem", (err, row) => {
-            console.log(row.id + ": " + row.info);
-        });
+function load(guid,callback){
+    var obj = {}
+    console.log("Loading " + guid)
+    connect((db,err) => {
+        db.each("SELECT * FROM proto WHERE guid = ?", [guid], 
+                (err, row) => {
+                    obj = JSON.parse(row.data)    
+                },
+                (err,rows) => {
+                    console.log("Found " + rows + " rows")
+                    db.close(() => {
+                        callback(obj)
+                    })
+                });
+    })    
+}
+
+function search(sql,params,callback){
+    var objs = []
+    console.log("Searching:" + sql)
+    connect((db,err) => {
+        db.each(sql, params, 
+                (err, row) => {
+                    objs.push(JSON.parse(row.data))    
+                },
+                (err,rows) => {
+                    console.log("Found " + rows + " rows")
+                    db.close(() => {
+                        callback(objs)
+                    })
+                });
     })
-    */
 }
 
-function search(){
+function del(guid,callback){
+    connect((db,err) => {
+        db.run("DELETE FROM proto WHERE guid = ?", [guid], 
+                () => {
+                    callback(this)    
+                });
+    })
 
 }
 
-function del(){
-
-}
-
-module.exports = {connect,init,load,save,search,del}
+module.exports = {path,load,save,search,del}
 
 /*
 
