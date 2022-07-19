5256eba2e7b6afdbdcbe64f991af0e018b614c4f
chris.lynch@sourceinsurance.co.uk <github@planetofthepenguins.com>
2022-07-19 12:52:27 +0000

    General purpose microservice

--- microservice.js
+++ microservice.js
@@ -1,6 +1,8 @@
 const app = require('./express.js')
 const winston = require('winston');
 const dotenv = require('dotenv').config()
+const fs = require('fs')
+const rabbit = require('./rabbit.js')
 
 const logConfiguration = {
     'transports': [
@@ -10,8 +12,23 @@ const logConfiguration = {
 
 const logger = winston.createLogger(logConfiguration);
 
-logger.info('Microservice initiated');
+logger.info('Microservice startup');
 
+// Start up any queues defined in the /queues folder
+const queues = {}
+fs.readdirSync(process.cwd() + '/queues').forEach(file => {
+    fileParts= file.split('.')
+    var queue = fileParts[0]
+    console.log(file);
+    queues[queue] = require(process.cwd() + '/queues/' + file)
+    if(queues[queue].consume){
+        logger.info("Starting consuming queue " + queue)
+        rabbit.consume(process.env['RABBIT_HOST'],queue,queues[queue].consume)
+    }
+});
+
+// Start up the Express server to deal with real time API requests.
+logger.info('Starting API Express server')
 app.ready()
 app.steady()
 app.go()
